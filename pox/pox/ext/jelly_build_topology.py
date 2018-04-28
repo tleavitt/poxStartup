26d304441bae45c628b25a656e8bbd6b4a0fc55e
tucker_leavitt <tucker.leavitt@gmail.com>
2018-04-28 14:42:57 -0700

    actually, HERE is the jellyfish topo

---
+++ pox/pox/ext/jelly_build_topology.py
@@ -0,0 +1,85 @@
+'''
+Warning! The order of the imports matters!
+Don't touch them if you don't have to!
+'''
+import os
+import sys
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.node import OVSController
+from mininet.node import Controller
+from mininet.node import RemoteController
+from mininet.cli import CLI
+sys.path.append("../../")
+from pox.ext.jelly_pox import JELLYPOX
+from subprocess import Popen
+from time import sleep, time
+
+
+from mininet.log import setLogLevel
+from mininet.clean import cleanup
+from mininet.util import dumpNodeConnections
+
+sys.path.append("../../")
+from pox.ext.jelly_pox import JELLYPOX
+from subprocess import Popen
+from time import sleep, time
+
+from jelly_graph import create_jellyfish_graph
+
+
+class JellyfishTopo( Topo ):
+    '''
+    Jellyfish topology with multiple links
+    '''
+    def __init__( self, N=20, k=12, r=4, verbose=False, **kwargs ):
+        Topo.__init__( self, **kwargs )
+        self.N = N; self.k = k; self.r = r
+        adjs = create_jellyfish_graph(N, k, r)
+
+        switches = [self.addSwitch('s{}'.format(i)) for i in range(N)]
+        servers = [
+            [self.addHost('h{}_{}'.format(i, j)) for j in range(r)]
+            for i in range(N)
+        ]
+        # servers[i] is the list of servers connected to switches[i]
+
+        # first, connect servers to switches
+        for sw, svs in zip(switches, servers):
+            for sv in svs:
+                if verbose:
+                    print "JF: adding link", sw, "<->", sv
+                self.addLink(sw, sv)
+
+        # connect switches according to the jellyfish adjacency lists.
+        for i, neighbors in enumerate(adjs):
+            for j in neighbors:
+                # only add links with i < j to avoid double counting.
+                if i < j:
+                    if verbose:
+                        print "JF: adding link", switches[i], "<->", switches[j]
+                    self.addLink(switches[i], switches[j])
+
+
+def runJellyfishLink():
+    '''
+    Create and run jellyfish network
+    '''
+    topo = JellyfishTopo(N=3, k=3, r=1, verbose=True)
+    net = Mininet(topo=topo, host=CPULimitedHost, link = TCLink, controller=JELLYPOX)
+    print "Dumping node connections"
+    dumpNodeConnections(net.hosts)
+    net.start()
+    net.pingAll()
+    net.stop()
+
+def main():
+    cleanup()
+    setLogLevel('info')
+    runJellyfishLink()
+
+if __name__ == "__main__":
+    main()
+
---
+++ pox/pox/ext/jelly_graph.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import random
+import pdb
+
+def create_jellyfish_graph(N=20, k=12, r=4):
+    '''
+    N: number of switches
+    k: ports per switch
+    r: ports per switch for servers
+    '''
+    d = k - r # degree of graph
+    adjs = [set() for _ in range(N)]
+    # adjs[i] is the set of neighbors of switch i
+    unfinished_nodes = set([i for i in range(N)]) # nodes with < d neighbors
+    finished_nodes = set() # nodes with d neighbors
+
+    def add_edge(i, j):
+        adjs[i].add(j)
+        adjs[j].add(i)
+
+    def remove_edge(i, j):
+        adjs[i].remove(j)
+        adjs[j].remove(i)
+
+    def check_unfin_to_fin(i):
+        if len(adjs[i]) == d and i in unfinished_nodes:
+            unfinished_nodes.remove(i)
+            finished_nodes.add(i)
+
+    def check_fin_to_unfin(i):
+        if len(adjs[i]) < d and i in finished_nodes:
+            finished_nodes.remove(i)
+            unfinished_nodes.add(i)
+
+    # randomly add neighbors
+    while len(unfinished_nodes) > 1:
+        '''
+        choose a random unfinished node i.
+        We're gonna add an edge to it, dammit!
+        '''
+        i = random.choice(tuple(unfinished_nodes))
+        unfinished_neighbors = unfinished_nodes - adjs[i] - set([i])
+        if len(unfinished_neighbors) > 0:
+            j = random.choice(tuple(unfinished_neighbors))
+            add_edge(i, j)
+            check_unfin_to_fin(i)
+            check_unfin_to_fin(j) 
+        else:
+            '''
+             i is neighbors with all unfinished nodes
+             => there are less than d unfinished nodes and they
+                are all neighbors with i
+            There is some j that is finished and is not neighbors
+            with i.
+            pick it, delete an edge from it, and add an edge i-j
+            '''
+            finished_neighbors = finished_nodes - adjs[i]
+            j = random.choice(tuple(finished_neighbors))
+            k = random.choice(tuple(adjs[j]))
+            remove_edge(j, k)
+            add_edge(j, i)
+            check_fin_to_unfin(k)
+            check_unfin_to_fin(i)
+
+
+    # after adding, there might be one node
+    # widh degree <= d - 2
+    if len(unfinished_nodes) == 1:
+        x = tuple(unfinished_nodes)[0]
+        while len(adjs[x]) <= d - 2:
+            i = random.choice(tuple(finished_nodes))
+            j = random.choice(tuple(adjs[i]))
+            remove_edge(i, j)
+            add_edge(i, x)
+            add_edge(x, j)
+            # check_unfin_to_fin(x)
+
+    return adjs
+
+def validate_adjs(adjs, d = 8):
+    for i, neighbors in enumerate(adjs):
+        assert len(neighbors) <= d
+        for j in neighbors:
+            assert i in adjs[j]
+
+if __name__ == '__main__':
+    N = 4; k = 4; r = 3
+    d = k - r
+    print "(N, k, r, d): ", (N, k, r, d)
+    adjs = create_jellyfish_graph(N, k, r)
+    validate_adjs(adjs, d = d)
+    print "Graph validation passed."
