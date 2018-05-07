de78bc700735f8102d3221e1ad65a79ec03f593b
jaspreetpandher <j031723@stanford.edu>
2018-05-07 16:36:32 -0700

    with adjacency created

--- pox/pox/ext/jelly_create_master_topology.py
+++ pox/pox/ext/jelly_create_master_topology.py
@@ -12,11 +12,13 @@ from pox.ext.routing_algorithms import ecmp_path_builder, ksp_path_builder, init
 
 from collections import defaultdict
 from jelly_graph import create_regular_jellyfish_graph, create_irregular_jellyfish_graph
+from jelly_build_topology import int2dpid
 import pdb
 
 sys.path.append("../../")
 from pox.forwarding.l2_multi_modified import Switch
 from pox.lib.addresses import EthAddr
+
 
 '''
 Goal of this file: initialize
@@ -26,6 +28,20 @@ and save the below config variables:
 # Adjacency map.  [dpid1][dpid2] -> port from dpid1 to dpid2
 adjacency = defaultdict(lambda:defaultdict(lambda:None))
 
+def adjsToAdjacency(adjs):
+    #mac_map = {}
+    port_map = {}
+
+    for key in adjs:
+        port_map[key] = 0
+        for key_dest in adjs[key]:
+            port_map[key] +=1
+
+
+            adjacency[int2dpid(key)][int2dpid(key_dest)] = port_map[key]
+
+
+    return adjacency
 # ethaddr -> (dpid, port)
 mac_map = {}
 
@@ -33,8 +49,11 @@ mac_map = {}
 is_switch = {}
 
 # (dpid, port) -> ethaddr
-idport_map = {}
+#idport_map = {}
 
+
+
+'''
 # [sw1][sw2] -> {distance: [possible next hops from sw1] }
 path_map = defaultdict(lambda:defaultdict(lambda:(None,None)))
 
@@ -42,7 +61,7 @@ our_path_map = init_path_map()
 
 # Select a routing algorithm:
 build_path_map = ecmp_path_builder(8) 
-
+'''
 
 
 
