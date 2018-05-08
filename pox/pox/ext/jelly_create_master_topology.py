687cc5cca6b7f422661229373fc2765a9384cc71
jaspreetpandher <j031723@stanford.edu>
2018-05-07 17:06:24 -0700

    Load topology

--- pox/pox/ext/jelly_create_master_topology.py
+++ pox/pox/ext/jelly_create_master_topology.py
@@ -12,7 +12,7 @@ from pox.ext.routing_algorithms import ecmp_path_builder, ksp_path_builder, init
 
 from collections import defaultdict
 from jelly_graph import create_regular_jellyfish_graph, create_irregular_jellyfish_graph
-from jelly_build_topology import int2dpid
+
 import pdb
 
 sys.path.append("../../")
@@ -24,9 +24,23 @@ from pox.lib.addresses import EthAddr
 Goal of this file: initialize
 and save the below config variables:
 '''
-
+def int2dpid( dpid ):
+   try:
+      dpid = hex( dpid )[ 2: ]
+      dpid = '0' * ( 16 - len( dpid ) ) + dpid
+      return dpid
+   except IndexError:
+      raise Exception( 'Unable to derive default datapath ID - '
+                       'please either specify a dpid or use a '
+               'canonical switch name such as s23.' )
 # Adjacency map.  [dpid1][dpid2] -> port from dpid1 to dpid2
 adjacency = defaultdict(lambda:defaultdict(lambda:None))
+#(dpid, port) -> ethaddr
+idport_map = {}
+mac_map = {}
+is_switch = {}
+
+
 
 def adjsToAdjacency(adjs):
     #mac_map = {}
@@ -43,13 +57,41 @@ def adjsToAdjacency(adjs):
 
     return adjacency
 # ethaddr -> (dpid, port)
-mac_map = {}
+
 
 # dpid -> boolean (True if switch, False if host)
-is_switch = {}
 
-# (dpid, port) -> ethaddr
-#idport_map = {}
+def make_is_switch(adjacency, N, n, r):
+	for dpid in adjacency:
+		if int(dpid) < N:
+			#its a switch
+			is_switch[dpid] = True
+
+		else :
+			is_switch[dpid] = False
+
+	return is_switch
+
+
+# Format is: most signifant byte: port number
+#            least significant bytes: dpid
+def dpid_port_to_mac(dpid, port):
+	msb = port & 0xff
+	lsbs = int(dpid) & 0x00FFffFFffFF
+	eth_addr = (msb << 40) | lsbs
+	return EthAddr("%012x" % eth_addr)
+
+def id_port_maps(adjacency):
+
+	for i, neighbors in adjacency.items():
+
+		for j, port in neighbors.items():
+			eth_addr = dpid_port_to_mac(i, port)
+			mac_map[eth_addr] = (i, port)
+			idport_map[(i, port)] = eth_addr
+
+	return mac_map, idport_map
+
 
 
 
