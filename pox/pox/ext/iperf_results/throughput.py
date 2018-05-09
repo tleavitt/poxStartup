6a3c30e7859f5e7bf41bd2b7e164d082a472a68b
jaspreetpandher <j031723@stanford.edu>
2018-05-09 09:48:59 -0700

    Calculations

--- pox/pox/ext/iperf_results/throughput.py
+++ pox/pox/ext/iperf_results/throughput.py
@@ -14,11 +14,14 @@ with open('ecmp8_N100_n10_r10.pretty', 'r') as f:
     rxbytes = 0
     for item in distros_dict:
     	for result in item['results']:
+    		#seconds = 0
+    		#txbytes = 0
+    		#rxbytes = 0
     		for dest_stats in result['destStats(seconds,txbytes,rxbytes)']:
     			seconds += dest_stats[0]
     			txbytes += dest_stats[1]
     			rxbytes += dest_stats[2]
-    print (seconds)
+    print(seconds)
     print(txbytes)
     print(rxbytes)
 
